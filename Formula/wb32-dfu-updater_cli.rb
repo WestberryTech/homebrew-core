37d1c8f5bf054e957a9155e44568dd9d8f317f0e
Joy <chang.li@westberrytech.com>
2021-12-09 16:59:58 +0800

    update License

--- Formula/wb32-dfu-updater_cli.rb
+++ Formula/wb32-dfu-updater_cli.rb
@@ -3,7 +3,7 @@ class DfuUtil < Formula
   homepage "https://github.com/WestberryTech/wb32-dfu-updater"
   url "https://github.com/WestberryTech/wb32-dfu-updater/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "33e7aeb616bdd8e4f4b92de9c7a15d35e0188683e18f2d394728e2c74798d900"
-  license "GPL-2.0-or-later"
+  license "Apache-2.0"
   revision 1
   head "https://github.com/WestberryTech/wb32-dfu-updater.git", branch: "main"
 
