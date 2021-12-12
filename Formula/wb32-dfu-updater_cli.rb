1a08ce94a51e9490b0bc7b9acbb5eacb4a07e0e2
wb-joy <chang.li@westberrytech.com>
2021-12-12 17:53:02 +0800

    Update

--- Formula/wb32-dfu-updater_cli.rb
+++ Formula/wb32-dfu-updater_cli.rb
@@ -2,7 +2,7 @@ class Wb32DfuUpdaterCli < Formula
   desc "USB programmer"
   homepage "https://github.com/WestberryTech/wb32-dfu-updater"
   url "https://github.com/WestberryTech/wb32-dfu-updater/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "56f3bfaa6b71db8cdecd776f5f9a3db58632bce060cf921e20f8529e1648d7c8"
+  sha256 "0cd44a4800f414f6e4c1675959cc514fd046c1920480500a5d8a3e08defb6b0e"
   license "Apache-2.0"
   head "https://github.com/WestberryTech/wb32-dfu-updater.git", branch: "master"
 
@@ -15,7 +15,6 @@ class Wb32DfuUpdaterCli < Formula
     libusb = Formula["libusb"]
     args << "-DLIBUSB_INCLUDE_DIRS=#{libusb.opt_include}/libusb-1.0"
     args << "-DLIBUSB_LIBRARIES=#{libusb.opt_lib}/#{shared_library("libusb-1.0")}"
-    args << "-DCMAKE_BUILD_TYPE=Release"
 
     mkdir "build" do
       system "cmake", "..", *args
