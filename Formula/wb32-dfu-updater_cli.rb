7c17e809ab347db9cc8e9ea6ef4eca6659bae1a5
wb-joy <chang.li@westberrytech.com>
2022-01-04 18:55:29 +0800

    Modify test

--- Formula/wb32-dfu-updater_cli.rb
+++ Formula/wb32-dfu-updater_cli.rb
@@ -23,7 +23,6 @@ class Wb32DfuUpdaterCli < Formula
   end
 
   test do
-    assert_match "wb32-dfu-updater ver: #{version}", shell_output(bin/"wb32-dfu-updater_cli -V")
-    assert_match "Not found device!", shell_output(bin/"wb32-dfu-updater_cli -l")
+    assert_match "wb32-dfu-updater ver: #{version}\nlibusb version 1.0.24 (11584)\n----------------------------------------\n", shell_output(bin/"wb32-dfu-updater_cli --list")
   end
 end
